62e94db80a4a9c33b01b485cb5d5554208403755
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-09-19 22:19:13 +0700

    ❇️ Add test for ETHRISE properties

---
+++ src/test/ETHRISE_Properties.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.7;
+pragma experimental ABIEncoderV2;
+
+import "lib/ds-test/src/test.sol";
+
+import {ETHRISE} from "../ETHRISE.sol";
+import {Token} from "../rToken.sol";
+
+contract ETHRISE_Properties is DSTest {
+    /// @notice Make sure all token properties is setup correctly
+    function test_ETHRISE_Properties() public {
+        // Initialize new market
+        ETHRISE ethrise = new ETHRISE();
+
+        // Make sure all properties are set
+        assertEq(ethrise.name(), "ETH 2x Leverage Risedle");
+        assertEq(ethrise.symbol(), "ETHRISE");
+        assertEq(ethrise.decimals(), 18); // It should set to default value
+
+        // Makure sure rETHRISE properties are set
+        Token rETHRISE = Token(address(ethrise.rToken()));
+        assertEq(rETHRISE.name(), "Risedle ETHRISE Supply Shares");
+        assertEq(rETHRISE.symbol(), "rETHRISE");
+        assertEq(rETHRISE.decimals(), 6); // Should be similar to the USDC
+        assertEq(rETHRISE.totalSupply(), 0); // Should be set to zero
+        assertEq(rETHRISE.admin(), address(ethrise)); // Only the market can mint & burn the rToken
+    }
+}
