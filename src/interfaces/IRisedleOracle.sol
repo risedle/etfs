97f1a1190b16f918f60edc1fafc868b3c917b455
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-11-11 03:58:37 +0700

    ✦ Update the IRisedleOracle docs

--- src/interfaces/IRisedleOracle.sol
+++ src/interfaces/IRisedleOracle.sol
@@ -5,5 +5,6 @@ pragma experimental ABIEncoderV2;
 
 interface IRisedleOracle {
     // Get price of the collateral based on the vault's underlying asset
+    // For example ETH that trade 4000 USDC is returned as 4000 * 1e6 because USDC have 6 decimals
     function getPrice() external view returns (uint256 price);
 }
