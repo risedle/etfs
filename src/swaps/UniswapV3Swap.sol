2601d2945418f90bd10c5a2b896b50149e9d01a5
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-11-12 01:54:22 +0700

    ✦ Add UniswapV3Swap contract and test

---
+++ src/swaps/UniswapV3Swap.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Risedle Uniswap V3 Swap contract
+// It implements IRisedleSwap interface.
+//
+// Copyright (c) 2021 Bayu - All rights reserved
+// github: pyk
+// email: bayu@risedle.com
+pragma solidity 0.8.9;
+pragma experimental ABIEncoderV2;
+
+import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { ISwapRouter } from "../interfaces/UniswapV3.sol";
+
+contract UniswapV3Swap {
+    using SafeERC20 for IERC20;
+
+    /// @notice The Uniswap V3 router address
+    address private router;
+
+    /// @notice The Uniswap V3 pool's fee (500, 3000, 10000)
+    uint24 private poolFee;
+
+    constructor(address uniswapRouter, uint24 fee) {
+        router = uniswapRouter;
+        poolFee = fee;
+    }
+
+    /**
+     * @notice Swap tokenIn to tokenOut
+     * @param tokenIn The ERC20 address of token that we want to swap
+     * @param tokenOut The ERC20 address of token that we want swap to
+     * @param maxAmountIn The maximum amount of tokenIn to get the tokenOut with amountOut
+     * @param amountOut The amount of tokenOut that we want to get
+     * @return amountIn The amount of tokenIn that we spend to get the amountOut of tokenOut
+     */
+    function swap(
+        address tokenIn,
+        address tokenOut,
+        uint256 maxAmountIn,
+        uint256 amountOut
+    ) external returns (uint256 amountIn) {
+        // Transfer the specified amount of tokenIn to this contract.
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), maxAmountIn);
+
+        // Approve Uniswap V3 router to spend maximum amount in
+        IERC20(tokenIn).safeApprove(router, maxAmountIn);
+
+        // Set the params, we want to get exact amount of collateral with minimal supply out as possible
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter.ExactOutputSingleParams({ tokenIn: tokenIn, tokenOut: tokenOut, fee: poolFee, recipient: msg.sender, deadline: block.timestamp, amountOut: amountOut, amountInMaximum: maxAmountIn, sqrtPriceLimitX96: 0 });
+
+        // Execute the swap
+        amountIn = ISwapRouter(router).exactOutputSingle(params);
+
+        // For exact output swaps, the maxAmountIn may not have all been spent
+        // If the actual amount spent (amountIn) is less than the specified maximum amount, we must refund the msg.sender and approve the swapRouter to spend 0.
+        if (amountIn < maxAmountIn) {
+            IERC20(tokenIn).safeApprove(router, 0);
+            IERC20(tokenIn).safeTransfer(msg.sender, maxAmountIn - amountIn);
+        }
+    }
+}
---
+++ src/test/swaps/UnsiwapV3Swap.t.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Risedle Uniswap V3 Swap test
+// Make sure the swap is working as expected
+
+pragma solidity 0.8.9;
+pragma experimental ABIEncoderV2;
+
+import "lib/ds-test/src/test.sol";
+
+import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { Hevm } from "../Hevm.sol";
+import { IRisedleOracle } from "../../interfaces/IRisedleOracle.sol";
+import { IRisedleSwap } from "../../interfaces/IRisedleSwap.sol";
+import { ChainlinkOracle } from "../../oracles/ChainlinkOracle.sol";
+import { UniswapV3Swap } from "../../swaps/UniswapV3Swap.sol";
+import { CHAINLINK_ETH_USD, CHAINLINK_USDC_USD, UNISWAPV3_SWAP_ROUTER, USDC_ADDRESS, WETH_ADDRESS } from "chain/Constants.sol";
+
+contract UniswapV3SwapTest is DSTest {
+    using SafeERC20 for IERC20;
+
+    /// @notice hevm utils to alter mainnet state
+    Hevm hevm;
+
+    function setUp() public {
+        hevm = new Hevm();
+    }
+
+    /// @notice Make sure the contract is working as expected
+    function test_SwapUSDCToWETH() public {
+        // Set the contract balance to 20K USDC
+        hevm.setUSDCBalance(address(this), 20000 * 1e6); // 20K USDC
+
+        // Use Uniswap V3 WETH/USDC 0.05% fee liquidity
+        UniswapV3Swap uniswap = new UniswapV3Swap(UNISWAPV3_SWAP_ROUTER, 500);
+
+        // Use Chainlink oracle
+        ChainlinkOracle oracle = new ChainlinkOracle(CHAINLINK_ETH_USD, CHAINLINK_USDC_USD, 6);
+
+        // Execute the borrow and swap
+        uint256 collateralAmount = 1 ether;
+        uint256 collateralPrice = IRisedleOracle(address(oracle)).getPrice();
+        // Maximum plus +1% from the oracle price
+        uint256 maximumCollateralPrice = collateralPrice + ((0.01 ether * collateralPrice) / 1 ether);
+        uint256 maxAmountIn = (collateralAmount * maximumCollateralPrice) / (1 ether);
+
+        // Allow swap contract to spend the the USDC
+        IERC20(USDC_ADDRESS).safeApprove(address(uniswap), maxAmountIn);
+
+        // Swap USDC to WETH
+        uint256 amountIn = IRisedleSwap(address(uniswap)).swap(USDC_ADDRESS, WETH_ADDRESS, maxAmountIn, collateralAmount);
+
+        // Make sure the amountIn is less than the maxAmountIn
+        assertLt(amountIn, maxAmountIn);
+
+        // Make sure this contract get the WETH
+        assertEq(IERC20(WETH_ADDRESS).balanceOf(address(this)), collateralAmount);
+
+        // Make sure the balance is updated
+        assertEq(IERC20(USDC_ADDRESS).balanceOf(address(this)), (20000 * 1e6) - amountIn);
+    }
+}
