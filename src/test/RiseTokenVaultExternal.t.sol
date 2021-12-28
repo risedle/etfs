04ae5922259b300057ee5f4643a9af8d8487b0b5
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-12-28 20:49:56 +0700

    ✦ test: ETHRISE rebalance in-range should be reverted

--- src/test/RiseTokenVaultExternal.t.sol
+++ src/test/RiseTokenVaultExternal.t.sol
@@ -817,6 +817,31 @@ contract RiseTokenVaultExternalTest is DSTest {
         vault.rebalance(address(unirise));
     }
 
+    function testFail_ETHRISERebalanceInRangeShouldBeReverted() public {
+        // Create new price oracles for the collateral
+        CustomizableOracle oracle = new CustomizableOracle();
+        oracle.setPrice(15 * 1e6); // Set UNI price to 15 USDC
+
+        // Create new swap contracts, with artificial slippage 0.05%
+        CustomizableSwap swapContract = new CustomizableSwap(address(oracle), 0.005 ether);
+        hevm.setUNIBalance(address(swapContract), 1_000_000 ether); // 1_000_000 UNI token
+        hevm.setUSDCBalance(address(swapContract), 1_000_000 * 1e6); // 1_000_000 USDC token
+
+        // Create new vaults for UNIRISE
+        uint256 initialPrice = 10 * 1e6; // Initial price is 10 USDC
+        (RiseTokenVault vault, RisedleERC20 unirise) = createNewVault(oracle, swapContract, false, UNI_ADDRESS, initialPrice);
+
+        // Create the dummy users
+        DummyUser user = new DummyUser(vault);
+        hevm.setUNIBalance(address(user), 10 ether);
+
+        // Mint UNIRISE
+        user.mintWithERC20(address(unirise), 10 ether); // Mint UNIRISE with 10 UNI
+
+        // Execute the rebalance; This should be failed
+        vault.rebalance(address(unirise));
+    }
+
     // Test Redeem
     // mint, no price change, then redeem, User should receive their collateral back minus 0.2% fee
     // mint, price go up, then redeem, User should receive their collateral back plus their profit
