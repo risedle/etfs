17f03d2bc4a8f88e8b313c525bf03c62f6c0dc8a
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-12-28 20:35:04 +0700

    ✦ test: ERC20RISE leveraging down

--- src/test/RiseTokenVaultExternal.t.sol
+++ src/test/RiseTokenVaultExternal.t.sol
@@ -745,6 +745,50 @@ contract RiseTokenVaultExternalTest is DSTest {
         vault.rebalance(address(ethrise));
     }
 
+    function test_ERC20RISELeverageDown() public {
+        // Create new price oracles for the collateral
+        CustomizableOracle oracle = new CustomizableOracle();
+        oracle.setPrice(15 * 1e6); // Set UNI price to 15 USDC
+
+        // Create new swap contracts, with artificial slippage 0.5%
+        CustomizableSwap swapContract = new CustomizableSwap(address(oracle), 0.005 ether);
+        hevm.setUNIBalance(address(swapContract), 1_000_000 ether); // 1_000_000 UNI token
+        hevm.setUSDCBalance(address(swapContract), 1_000_000 * 1e6); // 1_000_000 USDC token
+
+        // Create new vaults for UNIRISE
+        uint256 initialPrice = 10 * 1e6; // Initial price is 10 USDC
+        (RiseTokenVault vault, RisedleERC20 unirise) = createNewVault(oracle, swapContract, false, UNI_ADDRESS, initialPrice);
+
+        // Create the dummy users
+        DummyUser user = new DummyUser(vault);
+        hevm.setUNIBalance(address(user), 10 ether);
+
+        // Mint UNIRISE
+        user.mintWithERC20(address(unirise), 10 ether); // Mint UNIRISE with 10 UNI
+
+        // Initial leverage ratio is 2.010050251X
+        assertEq(vault.getLeverageRatioInEther(address(unirise)), 2010050200000000000);
+
+        // Set UNI price to go down, then leverage ratio will go up
+        oracle.setPrice(11.2 * 1e6);
+
+        // Validate NAV and leverage ratio
+        assertEq(vault.getNAV(address(unirise)), 4907873);
+        assertEq(vault.getLeverageRatioInEther(address(unirise)), 3058020246245165675); // 3.1x
+
+        // Execute the rebalance
+        vault.rebalance(address(unirise));
+
+        // The nav should not change
+        assertEq(vault.getNAV(address(unirise)), 4915198);
+
+        // The leverage ratio should change
+        assertEq(vault.getLeverageRatioInEther(address(unirise)), 2755400291097123656); // 2.7x
+
+        // The UNIRISE debt should be increased
+        assertEq(vault.getOutstandingDebt(address(unirise)), 128646224);
+    }
+
     // Test Redeem
     // mint, no price change, then redeem, User should receive their collateral back minus 0.2% fee
     // mint, price go up, then redeem, User should receive their collateral back plus their profit
